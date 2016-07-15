afabacb3223418388996949fd179d2f989281331
secretlyabanana <secretlyabananas@gmail.com>
2016-07-15 15:13:57 -0400

    Create clua multithreading.cpp

---
+++ coroutines/clua multithreading.cpp
@@ -0,0 +1,11 @@
+static int Delay(lua_State *L) {
+	Sleep(lua_tonumber(L, 1));
+	return 0;
+};
+
+int main() {
+  lua_State *L = luaL_newstate();
+  lua_register(L, "Delay", Delay);
+  
+  return 0;
+};
